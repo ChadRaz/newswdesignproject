652dd260d47bd0dbe851da63a80c1a1dbb533eab
alexj-7827 <alexjmnz2005@gmail.com>
2025-11-29 14:23:22 -0600

    Forth version of report.js.
    Added attendance record printing blocks for both "volunteer" and "event" functions. There's stil no connection to the database yet.

--- report.js
+++ report.js
@@ -1,8 +1,10 @@
 // This file was originally written by Alex Jimenez.
 const PDFDocument = require('pdfkit');
 
+// Get a connection to the database using fetch().
+
 // Volunteer block. Used when adding volunteers.
-function volunteer(doc, name, id, skills, pref){
+function volunteer(doc, name, id, skills, pref, attended, noShow){
 	doc.font(Helvetica, 14);
 	doc.table({
 		columnStyles: ["*", "*", "*", "*"], // Set up 4 columns.
@@ -19,11 +21,41 @@ function volunteer(doc, name, id, skills, pref){
             [{colSpan: 4, text: "PREFERENCES: " + pref}]
 		],
 	});
-    // Query for events attended by the volunteer. Use id.
+    doc
+        .underline(true)
+        .text("ATTENDANCE")
+        .underline(false);
+    if (attended.length() === 0) doc.text(name + "hasn't volunteered at any events yet.")
+    else for(let i = 0; i < attended.length(); i ++){
+        // Query the database. Use the event's ID.
+        let eName; // Event Name
+        let eID; // Event ID
+        let eDate; // Event Date
+        let eTime; // Event Time
+        let vRole; // Assigned Role
+
+        doc.text(eName + " [" + eID + "]: " + eDate + " " + eTime);
+        doc.list("ASSIGNED ROLE: " + vRole);
+    }
+    if (noShow.length() > 0){
+        doc
+            .underline(true)
+            .text("NO SHOW: " + noShow.length() + " EVENT(S)")
+            .underline(false);
+        for(let i = 0; i < noShow.length(); i ++){
+            // Query the database. Use the event's ID.
+            let eName; // Event Name
+            let eID; // Event ID
+            let eDate; // Event Date
+            let eTime; // Event Time
+
+            doc.text(eName + " [" + eID + "]: " + eDate + " " + eTime);
+        }
+    }
 }
 
 // Event block. Used when adding events.
-function event(doc, name, id, date, time, urgency, skills, desc){
+function event(doc, name, id, date, time, urgency, skills, desc, attendance){
 	doc.table({
 		columnStyles: ["*", "*", "*", "*"], // Set up 4 columns.
 		rowStyles: (i) => {
@@ -41,6 +73,21 @@ function event(doc, name, id, date, time, urgency, skills, desc){
 		],
 	});
     // Query for attendance record of event. Use id.
+    doc
+        .underline(true)
+        .text("ATTENDANCE")
+        .underline(false);
+    if (attendance.length() === 0) doc.text(name + "N/A.")
+    else for(let i = 0; i < attendance.length(); i ++){
+        // Query the database. Use the event's ID.
+        let eName; // Event Name
+        let eID; // Event ID
+        let eDate; // Event Date
+        let eTime; // Event Time
+        let vRole; // Assigned Role
+
+        doc.text(eName + " [" + eID + "]");
+    }
 }
 
 // For the update subtitle.
@@ -86,7 +133,8 @@ else for(let i = 0; i < volunteerList.length(); i ++){ // Volunteers are listed 
     let preferences = "None";
     if (volunteerList[i].preferences().length() >= 1) preferences = String(volunteerList[i].preferences());
 	volunteer(doc, volunteerList[i].name, volunteerList[i].id,
-        volunteerList[i].skills, preferences)
+        volunteerList[i].skills, preferences, volunteerList[i].attendance, 
+        volunteerList[i].noShow);
     doc.moveDown();
 }
 
@@ -110,7 +158,8 @@ else for(let i = eventList.length() - 1; i > -1; i --){
     if (eventList[i].desc().length() >= 1) description = String(eventList[i].desc());
     //function event(doc, name, id, date, time, urgency, skills, desc){
 	event(doc, eventList[i].name, eventList[i].id, eventList[i].date,
-        eventList[i].time, eventLit[i].urgency, eventList[i].skills, description);
+        eventList[i].time, eventLit[i].urgency, eventList[i].skills,
+        description, eventList[i].attendance);
     doc.moveDown();
 }
 
